189691cc95efe620298e8550993b206109ca64c9
unknown <kris.hoerth@gmail.com>
2022-09-29 15:58:10 -0300

    empece el menu

---
+++ components/ui/MenuPrincipal/MenuPrincipal.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from "./MenuPrincipal.module.scss"
+
+const MenuPrincipal = () => {
+return (
+    <nav className={styles.nav}>
+        <ul>
+            <li>link 1</li>
+            <li>link 2</li>
+            <li>link 3</li>
+            <li>link 4</li>
+        </ul>
+    </nav>
+)
+}
+
+export default MenuPrincipal
