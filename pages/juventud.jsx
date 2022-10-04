283b22e5132c98696a23878bf51d001aceabdf57
unknown <mer.estrella1@gmail.com>
2022-10-04 18:00:04 -0300

    juventud

--- pages/juventud.jsx
+++ pages/juventud.jsx
@@ -1,14 +1,15 @@
-import Head from 'next/head'
-import React from 'react'
-import MainContainer from '../components/Containers/MainContainer/MainContainer'
-import { Footer } from '../components/ui/Footer/Footer'
-import styles from "../styles/Juventud.module.scss"
+import Head from "next/head";
+import React from "react";
+import MainContainer from "../components/Containers/MainContainer/MainContainer";
+import { Footer } from "../components/ui/Footer/Footer";
+import { getJuventud } from "../services/queries/GetJuventud";
+import styles from "../styles/Juventud.module.scss";
 
-const juventud = () => {
+export default function Juventud() {
   return (
     <>
       <Head>
-        <title>Polo Obrero - Medios</title>
+        <title>Juventud del Polo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainContainer>
@@ -16,7 +17,5 @@ const juventud = () => {
       </MainContainer>
       <Footer />
     </>
-  )
+  );
 }
-
-export default juventud
