13a9cf2ca283c2275e6da2bacdb25c099ffc2786
Federico Albornoz <alborfede@gmail.com>
2022-10-11 18:07:27 -0300

    Pagina medios

--- pages/medios.jsx
+++ pages/medios.jsx
@@ -3,25 +3,38 @@ import React from 'react'
 import MainContainer from '../components/Containers/MainContainer/MainContainer'
 import { Footer } from '../components/ui/Footer/Footer'
 import styles from "../styles/Medios.module.scss"
+import { ListaYouTube } from "../components/ui/ListaYouTube/ListaYouTube";
+import { getVideosByPlayList } from "../services/queries/GetVideosByPlayList";
 
-const medios = () => {
+export default function Medios({
+  listaYouTube,
+}) {
+  console.log( listaYouTube );
   return (
     <>
       <Head>
         <title>Polo Obrero - Medios</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <MainContainer>
-        <h1 className={styles.titulo}>El Polo Obrero en los medios</h1>
-        <div className={styles.videos}>
-          <iframe width="100%" height="315" src="https://www.youtube.com/embed/_jj3M8PZOqE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          <iframe width="100%" height="315" src="https://www.youtube.com/embed/iwp6KiQe_Dg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          <iframe width="100%" height="315" src="https://www.youtube.com/embed/PNd5euXK-7g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-      </MainContainer>
+      <main>
+        <MainContainer>
+          <h1 className={styles.titulo}>El Polo Obrero en los medios</h1>
+          <ListaYouTube listaYouTube={listaYouTube} />
+        </MainContainer>
+      </main>
       <Footer />
     </>
-  )
+  );
 }
 
-export default medios
+export async function getServerSideProps() {
+  const listaYouTube = await getVideosByPlayList(
+    "PLcZulwVPWcU11toaBlOAHkjsRtgkg8Y-y",
+    9
+  );
+  return {
+    props: {
+      listaYouTube,
+    },
+  };
+}
