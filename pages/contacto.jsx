8cba038b1db8e8d103d0fa1bb45f021bc82b4ccd
juanv87 <juanv87@gmail.com>
2022-10-14 10:38:11 -0300

    FIX nombre de archivo Contacto.module.scss

--- pages/contacto.jsx
+++ pages/contacto.jsx
@@ -1,23 +1,23 @@
-import Head from 'next/head'
-import React from 'react'
-import MainContainer from '../components/Containers/MainContainer/MainContainer'
-import { Footer } from '../components/ui/Footer/Footer';
+import Head from "next/head";
+import React from "react";
+import MainContainer from "../components/Containers/MainContainer/MainContainer";
+import { Footer } from "../components/ui/Footer/Footer";
 import styles from "../styles/Contacto.module.scss";
 
 const contacto = () => {
   return (
     <>
-    <Head>
-      <title>Contacto</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <MainContainer>
-      <h1 className={styles.titulo}>Sumate a luchar con el Polo Obrero</h1>
-      <p className={styles.texto}>Incluir acá iFrame de google form</p>
-    </MainContainer>
-    <Footer />
+      <Head>
+        <title>Contacto</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MainContainer>
+        <h1 className={styles.titulo}>Sumate a luchar con el Polo Obrero</h1>
+        <p className={styles.texto}>Incluir acá iFrame de google form</p>
+      </MainContainer>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default contacto
+export default contacto;
