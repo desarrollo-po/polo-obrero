4c446204f82c1fb1983ecef017bcd707b072d439
juanv87 <juanv87@gmail.com>
2022-10-14 10:33:29 -0300

    Corrijo nombre de archivo IconTextos. Cambio a StaticProps la home

---
+++ pages/api/revalidate-home.js
@@ -0,0 +1,17 @@
+// import { getAllPosts } from "../../services/queries/Posts";
+
+export default async function handler(req, res) {
+  // Check for the secret token
+  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  try {
+    await res.revalidate("/");
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send("Error revalidating");
+  }
+}
