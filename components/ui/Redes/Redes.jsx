44aa482139d7959feb241fb6b95d7b96d8ed6457
unknown <mer.estrella1@gmail.com>
2022-10-20 17:43:37 -0300

    cambios en header mobile y tablet

--- components/ui/Redes/Redes.jsx
+++ components/ui/Redes/Redes.jsx
@@ -6,15 +6,17 @@ import styles from "./redes.module.scss";
 export const Redes = () => {
   return (
     <div className={styles.redes}>
-      <li>
+      <li className={styles.fb}>
         <a href="https://www.instagram/polo.obrero">
           <IconFacebook size="30" />
         </a>
       </li>
       <li>
-        <a href="https://www.facebook.com/polo.obrero.3">
-          <IconInstagram size="30" />
-        </a>
+        <div className={styles.ig}>
+          <a href="https://www.facebook.com/polo.obrero.3">
+            <IconInstagram size="30" />
+          </a>
+        </div>
       </li>
     </div>
   );
