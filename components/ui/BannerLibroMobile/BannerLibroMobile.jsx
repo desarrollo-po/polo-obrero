71595393acc93d7fe570f51b138db524215006fb
fdalbnz <alborfede@gmail.com>
2022-10-25 17:52:13 -0300

    Link en banners Libro

--- components/ui/BannerLibroMobile/BannerLibroMobile.jsx
+++ components/ui/BannerLibroMobile/BannerLibroMobile.jsx
@@ -5,7 +5,7 @@ export const BannerLibroMobile = () => {
   return (
     <section className={styles.bannerLibro}>
       <picture className={styles.imagenContainer}>
-        <a href="/contacto">
+        <a href="https://revistaedm.com/producto/preventa-una-historia-del-movimiento-piquetero/">
           <img src="../../../imgs/BANNER-HDMP-MOBILE.png" />
         </a>
       </picture>
--- components/ui/BannerLibroWeb/BannerLibroWeb.jsx
+++ components/ui/BannerLibroWeb/BannerLibroWeb.jsx
@@ -5,7 +5,7 @@ export const BannerLibroWeb = () => {
   return (
     <section className={styles.bannerLibro}>
       <picture className={styles.imagenContainer}>
-        <a href="/contacto">
+        <a href="https://revistaedm.com/producto/preventa-una-historia-del-movimiento-piquetero/">
           <img src="../../../imgs/BANNER-HDMP-WEB.png" />
         </a>
       </picture>
