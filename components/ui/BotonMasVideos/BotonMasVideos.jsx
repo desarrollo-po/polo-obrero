b9fbada007e8893590eee34580af64544f037a22
unknown <kris.hoerth@gmail.com>
2022-10-26 16:08:39 -0300

    pagina historia

--- components/ui/BotonMasVideos/BotonMasVideos.jsx
+++ components/ui/BotonMasVideos/BotonMasVideos.jsx
@@ -4,7 +4,7 @@ import styles from "./BotonMasVideos.module.scss";
 export const BotonMasVideos = () => {
   return (
     <div className={styles.btn}>
-      <a href="/medios">Ver más videos</a>
+      <a href="/videos">Ver más videos</a>
     </div>
   )
 }
---
+++ pages/historia.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import React from 'react'
+
+export const historia = () => {
+  return (
+    < >
+      <Head>
+        <title>Historia</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MainContainer>
+        Historia del Polo Obrero
+      </MainContainer>
+      <footer />
+    </>
+  )
+}
