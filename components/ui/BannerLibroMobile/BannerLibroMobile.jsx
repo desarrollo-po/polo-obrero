3c1e84478f377b140e58f00ad204c48a952debb5
unknown <mer.estrella1@gmail.com>
2022-10-28 17:13:55 -0300

    diseño menu principal version mobile

--- components/ui/BannerLibroMobile/BannerLibroMobile.jsx
+++ components/ui/BannerLibroMobile/BannerLibroMobile.jsx
@@ -6,7 +6,7 @@ export const BannerLibroMobile = () => {
     <section className={styles.bannerLibro}>
       <picture className={styles.imagenContainer}>
         <a href="https://revistaedm.com/producto/preventa-una-historia-del-movimiento-piquetero/">
-          <img src="../../../imgs/BANNER-HDMP-mobile.png" />
+          <img src="https://po-proyecto.tk/wp-content/uploads/2022/10/BANNER-HDMP-MOBILE-1.png" />
         </a>
       </picture>
     </section>
