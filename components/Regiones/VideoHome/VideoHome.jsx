71e7f08af64937bb333d2327d46e09f6feafe5bc
unknown <mer.estrella1@gmail.com>
2022-11-03 16:34:03 -0300

    video home, cambios

--- components/Regiones/VideoHome/VideoHome.jsx
+++ components/Regiones/VideoHome/VideoHome.jsx
@@ -6,6 +6,7 @@ const VideoHome = () => {
   return (
     <>
       <p className={styles.texto1}>LUCHAMOS POR TRABAJO GENUINO</p>
+
       <div className={styles.containerVideo}>
         <video
           src="https://po-proyecto.tk/wp-content/uploads/2022/11/y2mate.com-Por-trabajo-genuino-Masiva-movilizacion-del-Polo-Obrero_1080p-online-video-cutter.com-1.mp4"
