f876109c768406b879b40932f8220ad032fddf71
merestrella <mer.estrella1@gmail.com>
2022-11-09 17:38:59 -0300

    agregue licencia en footer y cree pagina idem

--- components/ui/Footer/Footer.jsx
+++ components/ui/Footer/Footer.jsx
@@ -31,6 +31,11 @@ export const Footer = () => {
             </li>
           </ul>
         </nav>
+        <div className={styles.contenedorLicense}>
+        <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"> <img alt="Licencia Creative Commons" src="https://i.creativecommons.org/l/by/4.0/80x15.png"></img> Polo Obrero está distribuido bajo una<br></br>Licencia Creative Commons Atribución 4.0 Internacional</a>
+        </div>
+        <a href="/licencia"><p className={styles.license}>Como usar esta licencia</p></a>
+        
       </footer>
     </MainContainer>
   );
