bcdae7df106b3a2b3eb5395fb69f6a63e8c37142
Federico Albornoz <alborfede@gmail.com>
2022-11-11 15:49:20 -0300

    Licencia mobile acomodada

--- components/ui/Footer/Footer.jsx
+++ components/ui/Footer/Footer.jsx
@@ -31,11 +31,14 @@ export const Footer = () => {
             </li>
           </ul>
         </nav>
+        <div className={styles.contenedorLicenses}>
         <div className={styles.contenedorLicense}>
-        <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"> <img alt="Licencia Creative Commons" src="https://i.creativecommons.org/l/by/4.0/80x15.png"></img> Polo Obrero está distribuido bajo una<br></br>Licencia Creative Commons Atribución 4.0 Internacional</a>
+          <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"> <img alt="Licencia Creative Commons" src="https://i.creativecommons.org/l/by/4.0/80x15.png"></img> Polo Obrero está distribuido bajo una<br></br>Licencia Creative Commons Atribución 4.0 Internacional</a>
         </div>
-        <a href="/licencia"><p className={styles.license}>Como usar esta licencia</p></a>
-        
+        <div className={styles.license}>
+          <a href="/licencia"><p className={styles.license}>Como usar esta licencia</p></a>
+        </div>
+        </div>
       </footer>
     </MainContainer>
   );
