0dabb86f7fd7714c71a2ee94d0957976721f41ce
unknown <kris.hoerth@gmail.com>
2022-11-29 14:19:54 -0300

    auditoria locales, cambio de menu, video ocultado

--- pages/auditoria-potenciar-trabajo.jsx
+++ pages/auditoria-potenciar-trabajo.jsx
@@ -2,21 +2,21 @@ import Head from 'next/head';
 import React from "react";
 import MainContainer from '../components/Containers/MainContainer/MainContainer';
 import { Footer } from '../components/ui/Footer/Footer';
-import styles from "../styles/Historia.module.scss";
+import styles from "../styles/Auditoria-potenciar-trabajo.module.scss";
 
-const historia = () => {
+const auditorias = () => {
   return (
     < >
       <Head>
-        <title>Historia</title>
+        <title>Auditoria Potenciar Trabajo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainContainer>
-      <h1 className={styles.titulo}>Historia del Polo Obrero</h1>
+      <h1 className={styles.titulo}>Auditoria Potenciar Trabajo</h1>
       </MainContainer>
       <Footer />
     </>
   )
 }
 
-export default historia
+export default auditorias
--- pages/locales.jsx
+++ pages/locales.jsx
@@ -2,21 +2,21 @@ import Head from 'next/head';
 import React from "react";
 import MainContainer from '../components/Containers/MainContainer/MainContainer';
 import { Footer } from '../components/ui/Footer/Footer';
-import styles from "../styles/Historia.module.scss";
+import styles from "../styles/Locales.module.scss";
 
-const historia = () => {
+const locales = () => {
   return (
     < >
       <Head>
-        <title>Historia</title>
+        <title>Locales</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainContainer>
-      <h1 className={styles.titulo}>Historia del Polo Obrero</h1>
+      <h1 className={styles.titulo}>Locales del Polo Obrero</h1>
       </MainContainer>
       <Footer />
     </>
   )
 }
 
-export default historia
+export default locales
