829016133d6c525abb776b635ac57601741fb501
unknown <kris.hoerth@gmail.com>
2022-12-01 14:10:31 -0300

    texto alt banner

--- components/ui/BannerAuditoriasMobile/BannerAuditoriasMobile.jsx
+++ components/ui/BannerAuditoriasMobile/BannerAuditoriasMobile.jsx
@@ -5,8 +5,8 @@ export const BannerAuditoriasMobile = () => {
   return (
     <section className={styles.bannerAuditoriasMobile}>
       <picture className={styles.imagenContainer}>
-        <a href="">
-          <img src="https://po-proyecto.tk/wp-content/uploads/2022/11/800-x-800.jpg" />
+      <a href="auditoria-potenciar-trabajo">
+          <img src="https://po-proyecto.tk/wp-content/uploads/2022/11/800-x-800.jpg" alt="auditoría potenciar trabajo preguntas consultas dudas datos" />
         </a>
       </picture>
     </section>
--- components/ui/BannerAuditoriasWeb/BannerAuditoriasWeb.jsx
+++ components/ui/BannerAuditoriasWeb/BannerAuditoriasWeb.jsx
@@ -5,7 +5,7 @@ export const BannerAuditoriasWeb = () => {
   return (
     <section className={styles.bannerAuditoriasWeb}>
       <picture className={styles.imagenContainer}>
-        <a href="">
+        <a href="auditoria-potenciar-trabajo">
           <img src="https://po-proyecto.tk/wp-content/uploads/2022/11/1200-x-480.jpg" />
         </a>
       </picture>
