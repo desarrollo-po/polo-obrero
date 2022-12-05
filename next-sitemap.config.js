d18d402ade4b172b84a8daf7d751ca5583236a77
eiguer <fedeeiguer@hotmail.com>
2022-12-05 16:25:27 -0300

    agrego sitemap y robot.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://poloobrero.org.ar',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+    sitemapSize : 7000 ,
+  }
