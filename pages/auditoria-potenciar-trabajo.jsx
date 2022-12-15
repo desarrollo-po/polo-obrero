2455c42524473bf4990f9daeabe8b3f89cd631d9
Federico Albornoz <alborfede@gmail.com>
2022-12-15 17:27:05 -0300

    numeros whatsapp

--- pages/auditoria-potenciar-trabajo.jsx
+++ pages/auditoria-potenciar-trabajo.jsx
@@ -35,120 +35,120 @@ const auditorias = () => {
         <p>Comunicate con nosotros por whatsapp para que te ayudemos en tu barrio o provincia y asi poder defender tu plan social</p>
         <div className={styles.contactos}>
           <ul>
-            <li className={styles.region}>CABA</li>
-            <li><a href="">1138962853</a></li>
+            <li className={styles.region} target="_blank">CABA</li>
+            <li><a href="https://wa.me/+5491138962853">1138962853</a></li>
           </ul>
           <ul>
-            <li className={styles.region}>ZONA SUR</li>
-            <li><a href="">1138962853</a></li>
+            <li className={styles.region} target="_blank">ZONA SUR</li>
+            <li><a href="https://wa.me/+5491138962853">1138962853</a></li>
           </ul>
           <ul>
             <li className={styles.region}>ZONA NORTE</li>
-            <li><a href="">1138962853</a></li>
+            <li><a href="https://wa.me/+5491138962853">1138962853</a></li>
           </ul>
           <ul>
             <li className={styles.region}>ZONA OESTE</li>
-            <li><a href="">1138962853</a></li>
+            <li><a href="https://wa.me/+5491138962853">1138962853</a></li>
           </ul>
           <ul>
             <li className={styles.region}>MATANZA</li>
-            <li><a href="">1138962853</a></li>
+            <li><a href="https://wa.me/+5491138962853">1138962853</a></li>
           </ul>
           <ul>
             <li className={styles.region}>BAHÍA BLANCA</li>
-            <li><a href="">2914328188</a></li>
+            <li><a href="https://wa.me/+5492914328188">2914328188</a></li>
           </ul>
           <ul>
             <li className={styles.region}>MAR DEL PLATA</li>
-            <li><a href="">1164217714</a></li>
+            <li><a href="https://wa.me/+5491164217714">1164217714</a></li>
           </ul>
           <ul>
             <li className={styles.region}>SAN NICOLAS</li>
-            <li><a href="">3364260825</a></li>
+            <li><a href="https://wa.me/+5493364260825">3364260825</a></li>
           </ul>
           <ul>
             <li className={styles.region}>LA PLATA</li>
-            <li><a href="">2215084221</a></li>
+            <li><a href="https://wa.me/+5492215084221">2215084221</a></li>
           </ul>
           <ul>
             <li className={styles.region}>ENSENADA</li>
-            <li><a href="">2215084221</a></li>
+            <li><a href="https://wa.me/+5492215084221">2215084221</a></li>
           </ul>
           <ul>
             <li className={styles.region}>BERISSO</li>
-            <li><a href="">2215084221</a></li>
+            <li><a href="https://wa.me/+5492215084221">2215084221</a></li>
           </ul>
           <ul>
             <li className={styles.region}>ENTRE RIOS (LA PAZ)</li>
-            <li><a href="">3437518914</a></li>
+            <li><a href="https://wa.me/+5493437518914">3437518914</a></li>
           </ul>
           <ul>
             <li className={styles.region}>ENTRE RIOS (PARANÁ)</li>
-            <li><a href="">3434065203</a></li>
+            <li><a href="https://wa.me/+5493434065203">3434065203</a></li>
             </ul>
           <ul>
             <li className={styles.region}>ENTRE RIOS (CONCEPCIÓN DEL URUGUAY)</li>
-            <li><a href="">3442459465</a></li>
+            <li><a href="https://wa.me/+5493442459465">3442459465</a></li>
           </ul>
           <ul>
             <li className={styles.region}>CHUBUT</li>
-            <li><a href="">2944217627</a></li>
+            <li><a href="https://wa.me/+5492944217627">2944217627</a></li>
           </ul>
           <ul>
             <li className={styles.region}>RÍO NEGRO</li>
-            <li><a href="">2984366812</a></li>
+            <li><a href="https://wa.me/+5492984366812">2984366812</a></li>
           </ul>
           <ul>
             <li className={styles.region}>TIERRA DEL FUEGO</li>
-            <li><a href="">2964668039</a></li>
+            <li><a href="https://wa.me/+5492964668039">2964668039</a></li>
           </ul>
           <ul>
             <li className={styles.region}>SANTA CRUZ</li>
-            <li><a href="">2975900928</a> 
+            <li><a href="https://wa.me/+5492975900928">2975900928</a> 
             <br></br>         
-            <a href="">2974252059</a>
+            <a href="https://wa.me/+5492974252059">2974252059</a>
             <br></br>
-            <a href="">2974167968</a></li>
+            <a href="https://wa.me/+5492974167968">2974167968</a></li>
           </ul>
           <ul>
             <li className={styles.region}>NEUQUEN</li>
-            <li><a href="">2994120889</a></li>
+            <li><a href="https://wa.me/+5492994120889">2994120889</a></li>
           </ul>
           <ul>
             <li className={styles.region}>RAMALLO</li>
-            <li><a href="">3364495071</a></li>
+            <li><a href="https://wa.me/+5493364495071">3364495071</a></li>
           </ul>
           <ul>
             <li className={styles.region}>NECOCHEA</li>
-            <li><a href="">1123474823</a></li>
+            <li><a href="https://wa.me/+5491123474823">1123474823</a></li>
           </ul>
           <ul>
             <li className={styles.region}>PERGAMINO</li>
-            <li><a href="">2477221298</a></li>
+            <li><a href="https://wa.me/+5492477221298">2477221298</a></li>
           </ul>
           <ul>
             <li className={styles.region}>TANDIL</li>
-            <li><a href="">249353597</a></li>
+            <li><a href="https://wa.me/+549249353597">249353597</a></li>
           </ul>
           <ul>
             <li className={styles.region}>FORMOSA</li>
-            <li><a href="">3704356449</a></li>
+            <li><a href="https://wa.me/+5493704356449">3704356449</a></li>
           </ul>
           <ul>
             <li className={styles.region}>MISIONES</li>
-            <li><a href="">3751330512</a></li>
+            <li><a href="https://wa.me/+5493751330512">3751330512</a></li>
           </ul>
           <ul>
             <li className={styles.region}>MENDOZA</li>
-            <li><a href="">2612401457</a></li>
+            <li><a href="https://wa.me/+5492612401457">2612401457</a></li>
           </ul>
           <ul>
             <li className={styles.region}>LA RIOJA</li>
-            <li><a href="">3804356066</a></li>
+            <li><a href="https://wa.me/+5493804356066">3804356066</a></li>
           </ul>
           <ul>
             <li className={styles.region}>SAN JUAN</li>
-            <li><a href="">2644838046</a></li>
+            <li><a href="https://wa.me/+5492644838046">2644838046</a></li>
           </ul>
         </div>
       </div>
