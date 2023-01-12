67c39857ce6086ef5433b851998249928166d335
unknown <mer.estrella1@gmail.com>
2023-01-12 19:27:59 -0300

    agrego fecha fin de auditoria

--- components/ui/BannerAuditoriasWeb/BannerAuditoriasWeb.jsx
+++ components/ui/BannerAuditoriasWeb/BannerAuditoriasWeb.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import styles from "../BannerAuditoriasWeb/auditoriasWeb.module.scss";
 
@@ -5,11 +6,16 @@ export const BannerAuditoriasWeb = () => {
   return (
     <section className={styles.bannerAuditoriasWeb}>
       <picture className={styles.imagenContainer}>
-        <a href="auditoria-potenciar-trabajo">
-          <img src="https://po-proyecto.tk/wp-content/uploads/2022/12/banners-polo-auditorias_1200-x-480.jpg" alt="auditoría potenciar trabajo preguntas consultas dudas datos" />
-        </a>
+        <Link href="auditoria-potenciar-trabajo">
+          <img
+            src="https://po-proyecto.tk/wp-content/uploads/2022/12/banners-polo-auditorias_1200-x-480.jpg"
+            alt="auditoría potenciar trabajo preguntas consultas dudas datos"
+          />
+        </Link>
       </picture>
+      <Link href="/auditoria-potenciar-trabajo">
+        <p className={styles.texto}>TENÉS TIEMPO HASTA EL 15/2</p>
+      </Link>
     </section>
   );
 };
-
