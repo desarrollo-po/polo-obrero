7f0ad2f77921f53f31e6b4e51cd1934c1437c035
eiguer <fedeeiguer@hotmail.com>
2023-04-10 17:00:30 -0300

    agrego videos prueba para po

---
+++ pages/videos-po.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import MainContainer from '../components/Containers/MainContainer/MainContainer'
+import styles from "../styles/Medios.module.scss"
+import { ListaYouTube } from "../components/ui/ListaYouTube/ListaYouTube";
+import { getVideosByPlayList } from "../services/queries/GetVideosByPlayList";
+
+export default function Medios({
+  listaYouTube,
+}) {
+  console.log( listaYouTube );
+  return (
+    <>
+      <main>
+        <MainContainer>
+          <div className={styles.containerTitulo}>
+          <h1 className={styles.titulo}>Nuestros videos</h1>
+          </div>
+          <ListaYouTube listaYouTube={listaYouTube} />
+          
+        <style jsx>{`
+        .HeaderContainer_container__txJg0 {
+            display: none;
+        }
+        `}</style>
+
+        </MainContainer>
+      </main>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const listaYouTube = await getVideosByPlayList(
+    "PLcZulwVPWcU2K9TOT8Qt-lgrDjU8EQMSP",
+    9
+  );
+  return {
+    props: {
+      listaYouTube,
+    },
+  };
+}
