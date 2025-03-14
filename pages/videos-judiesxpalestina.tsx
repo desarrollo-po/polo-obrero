958144d5c3c6c421af7e08f984a0af0fcdd40330
merestrella <mer.estrella1@gmail.com>
2025-03-14 16:15:31 -0300

    videos judies

--- pages/videos-judiesxpalestina.tsx
+++ pages/videos-judiesxpalestina.tsx
@@ -20,7 +20,60 @@ export default function Medios({ listaYouTubeJudies }) {
           <ListaYouTube listaYouTube={listaYouTubeJudies} />{" "}
         </Box>
       </main>
+      <style jsx global>{`
+        header {
+          display: none !important;
+        }
+        main {
+          background-color: white;
+        }
+        .MuiButton-root {
+          margin-bottom: 20px;
+        }
+        html,
+        body {
+          background-color: white !important;
+        }
+        .css-69z67c-MuiTabs-scroller {
+          left: auto;
+          display: flex;
+          justify-content: center;
+        }
+        .MuiTabs-flexContainer.css-k008qs {
+          align-items: center;
+          justify-content: center;
+        }
 
+        @media only screen and (max-width: 700px) {
+          header {
+            display: none !important;
+          }
+          main {
+            background-color: white !important;
+            width: 100%;
+          }
+          .MuiButton-root {
+            margin-bottom: 20px;
+            left: 1%;
+          }
+          .ListaYouTube-po_listaYouTube__nKWDq {
+            margin: auto !important;
+          }
+          .MuiTabs-flexContainer.MuiTabs-centered.css-1wf8b0h-MuiTabs-flexContainer {
+            justify-content: center;
+            gap: 0px;
+          }
+          html,
+          body {
+            background-color: white !important;
+          }
+          .css-13xfq8m-MuiTabPanel-root {
+          }
+          .MuiTabs-flexContainer.css-k008qs {
+            width: max-content;
+          }
+        }
+      `}</style>
     </>
   );
 }
